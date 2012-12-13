b8a77aae7634906dffc013ce1d6aaf962ddbc34d
BBenj <balga.benjamin@gmail.com>
2012-12-13 21:53:47 +0100

    First tests with pySerial with port listing

---
+++ pyterm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, sys
+from serial import Serial
+
+
+s = Serial('/dev/cu.usbserial-A6008cB6')
+
+#print("Open: ", s.open())
+
+print("Bytes: ", s.write(bytes('test', 'UTF-8')))
+
+print("Read: ", s.read(4))
+
+print("Close: ", s.close())
