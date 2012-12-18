f3cb5de725ed03da20512d4d10d0afef09f19a19
BBenj <balga.benjamin@gmail.com>
2012-12-18 19:17:59 +0100

    PySide tests: ports listing in ComboBox with signal

--- pyterm.py
+++ pyterm.py
@@ -3,22 +3,53 @@
 
 import os, sys
 from serial import Serial
-
-
-s = Serial('/dev/cu.usbserial-A6008cB6')
-
-#print("Open: ", s.open())
-
-print("Bytes: ", s.write(bytes('test', 'UTF-8')))
-
-print("Read: ", s.read(4))
-
-print("Close: ", s.close())
-
-
+from PySide import QtGui, QtCore
+from PySide.QtCore import *
+from PySide.QtGui import *
 from portsListener import PortsListener
 
-for port in PortsListener.getPorts():
-	print("port: ", port)
 
-#execfile("list_ports.py")
+
+def main(args):
+	
+	def portChanged(portName):
+		print('Port changed: ', portName)
+	
+	a = QtGui.QApplication(args)
+	
+	
+	s = Serial('/dev/cu.usbserial-A6008cB6')
+	
+	#print("Open: ", s.open())
+	
+	print("Bytes: ", s.write(bytes('test', 'UTF-8')))
+	
+	print("Read: ", s.read(4))
+	
+	s.close()
+	
+	matchedPorts = PortsListener.getPorts()
+	
+	ports_cb = QtGui.QComboBox()
+	ports_cb.currentIndexChanged[str].connect(portChanged)
+	
+	for port in matchedPorts:
+		ports_cb.addItem(port)	# TODO: add QVariant with port data
+	
+	
+	window =  QWidget()
+	layout =  QVBoxLayout()
+	
+	layout.addWidget(ports_cb)
+	
+	window.setLayout(layout)
+	window.show()
+	
+	
+	r = a.exec_()
+	return r
+
+
+if __name__ == "__main__":
+	main(sys.argv)
+	
